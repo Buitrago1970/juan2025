1c77b799f9e564a13248ae144ce126345cdb749c
Juan Buitrago <juanbuitrago2002@outlook.com>
2024-12-10 15:11:50 -0500

    Enhance AboutMe component with GSAP animations and update styles for highlighted text

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,5 +1,5 @@
-import { useRef } from "react";
-
+import { useEffect, useRef } from "react";
+import gsap from 'gsap';
 import styles from '@/styles/AboutMe.module.css';
 import Cards from "./Cards";
 
@@ -8,10 +8,63 @@ interface AboutMeProps {
 }
 
 const AboutMe: React.FC<AboutMeProps> = ({ id }) => {
-  const textRef = useRef(null);
+  const textRef = useRef<HTMLHeadingElement>(null);
+  const highlightRefs = useRef<(HTMLSpanElement | null)[]>([]);
 
-  const sentence =
-    "I have dedicated my career to developing web applications that are not only functional but also charming and intuitive.";
+  useEffect(() => {
+    if (textRef.current) {
+      // Animación principal del texto
+      gsap.fromTo(textRef.current,
+        { y: 50, opacity: 0 },
+        {
+          y: 0,
+          opacity: 1,
+          duration: 1,
+          ease: "power2.out"
+        }
+      );
+
+      // Animación de las palabras resaltadas
+      highlightRefs.current.forEach((highlight, index) => {
+        if (highlight) {
+          // Animación inicial de fade in
+          gsap.fromTo(highlight,
+            { opacity: 0 },
+            {
+              opacity: 1,
+              duration: 0.5,
+              delay: 0.2 * index,
+            }
+          );
+
+          // Animación del efecto de brillo
+          gsap.fromTo(highlight.querySelector('::after'),
+            { 
+              x: '-100%',
+              y: '-100%'
+            },
+            {
+              x: '100%',
+              y: '100%',
+              duration: 1,
+              delay: 0.5 + (0.2 * index),
+              ease: "power2.inOut",
+            }
+          );
+        }
+      });
+    }
+  }, []);
+
+  const sentence = (
+    <>
+      I have dedicated my career to developing{' '}
+      <span ref={el => { highlightRefs.current[0] = el }} className={styles.highlight}>web applications</span> that are not only{' '}
+      <span ref={el => {highlightRefs.current[1] = el}} className={styles.highlight}>functional</span> but also{' '}
+      <span ref={el => {highlightRefs.current[2] = el}} className={styles.highlight}>charming</span> and{' '}
+      <span ref={el => { highlightRefs.current[3] = el; }} className={styles.highlight}>intuitive</span>.
+    </>
+  );
 
   return (
     <section id={id} className="mt-14 lg:mt-52 lg:mr-5 mb-32">
